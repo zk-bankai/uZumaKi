e6803b06411c1a1920ab9abfd6ea8f3d3c44ac49
aniket-prajapati <anikethprajapati@gmail.com>
2024-03-04 15:17:02 +0530

    restructured code

--- stone_stark/src/bench.rs
+++ stone_stark/src/bench.rs
@@ -1,5 +1,5 @@
 use std::path::Path;
-
+use std::path::PathBuf;
 use crate::error::{ProverError, VerifierError};
 
 pub fn run_prover_from_command_line(
@@ -61,152 +61,218 @@ pub fn run_verifier_from_command_line(
     Ok(())
 }
 
+fn run_test_case(
+    test_category: &str,
+    test_name: &str,
+    prover_json: &str,
+    verifier_json: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let base_path = PathBuf::from(format!("./data/{}/{}", test_category, test_name));
+    let benchmarks_path = PathBuf::from("../benchmarks/stone");
+
+    run_prover_from_command_line(
+        &base_path.join("public_input.json"),
+        &base_path.join("private_input.json"),
+        &base_path.join("cpu_air_prover_config.json"),
+        &base_path.join("cpu_air_params.json"),
+        &base_path.join("proof.json"),
+        &benchmarks_path.join(prover_json),
+        1,
+    )?;
+
+    run_verifier_from_command_line(
+        &base_path.join("proof.json"),
+        &benchmarks_path.join(verifier_json),
+        1,
+    )?;
+
+    Ok(())
+}
+
+
 #[cfg(test)]
 mod test {
-    use rstest::rstest;
     use super::*;
+    use rstest::rstest;
 
     #[rstest]
-    fn sha256_8bytes_test_run_prover_verifier_from_command_line() {
-        run_prover_from_command_line(
-            Path::new("./data/sha256/8bytes/public_input.json"),
-            Path::new("./data/sha256/8bytes/private_input.json"),
-            Path::new("./data/sha256/8bytes/cpu_air_prover_config.json"),
-            Path::new("./data/sha256/8bytes/cpu_air_params.json"),
-            Path::new("./data/sha256/8bytes/proof.json"),
-            Path::new("../data/stone/sha256/8bytes/prover.json"),
-            1
-        )
-        .unwrap();
-
-        // let proof = read_proof_file(output_file.path());
-        // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
-
-        run_verifier_from_command_line(
-            Path::new("./data/sha256/8bytes/proof.json"), 
-            Path::new("../data/stone/sha256/8bytes/verifier.json"),
-            1
-        )
-        .expect("Proof file is valid");
+    fn sha256_8bytes_test() -> Result<(), Box<dyn std::error::Error>> {
+        run_test_case("sha256", "8bytes", "sha256/8B-prover.json", "sha256/8B-verifier.json")
     }
 
     #[rstest]
-    fn fibonacci_1_test_run_prover_verifier_from_command_line() {
-        run_prover_from_command_line(
-            Path::new("./data/fibonacci/fib1/fib1_public_input.json"),
-            Path::new("./data/fibonacci/fib1/fib1_private_input.json"),
-            Path::new("./data/fibonacci/fib1/cpu_air_prover_config.json"),
-            Path::new("./data/fibonacci/fib1/cpu_air_params.json"),
-            Path::new("./data/fibonacci/fib1/fib1_proof.json"),
-            Path::new("../data/stone/fibonacci/fib1-prover.json"),
-            1
-        )
-        .unwrap();
-
-        // let proof = read_proof_file(output_file.path());
-        // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
-
-        run_verifier_from_command_line(
-            Path::new("./data/fibonacci/fib1/fib1-proof.json"), 
-            Path::new("../data/stone/fibonacci/fib1-verifier.json"),
-            1
-        )
-        .expect("Proof file is valid");
+    fn fibonacci_1_test() -> Result<(), Box<dyn std::error::Error>> {
+        run_test_case("fibonacci", "fib1", "fibonacci/fib1-prover.json", "fibonacci/fib1-verifier.json")
     }
 
     #[rstest]
-    fn fibonacci_10_test_run_prover_verifier_from_command_line() {
-        run_prover_from_command_line(
-            Path::new("./data/fibonacci/fib10/fib10_public_input.json"),
-            Path::new("./data/fibonacci/fib10/fib10_private_input.json"),
-            Path::new("./data/fibonacci/fib10/cpu_air_prover_config.json"),
-            Path::new("./data/fibonacci/fib10/cpu_air_params.json"),
-            Path::new("./data/fibonacci/fib10/fib10_proof.json"),
-            Path::new("../data/stone/fibonacci/fib10-prover.json"),
-            1
-        )
-        .unwrap();
-
-        // let proof = read_proof_file(output_file.path());
-        // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
-
-        run_verifier_from_command_line(
-            Path::new("./data/fibonacci/fib10/fib10-proof.json"), 
-            Path::new("../data/stone/fibonacci/fib10-verifier.json"),
-            1
-        )
-        .expect("Proof file is valid");
+    fn fibonacci_10_test() -> Result<(), Box<dyn std::error::Error>> {
+        run_test_case("fibonacci", "fib10", "fibonacci/fib10-prover.json", "fibonacci/fib10-verifier.json")
     }
 
     #[rstest]
-    fn fibonacci_100_test_run_prover_verifier_from_command_line() {
-        run_prover_from_command_line(
-            Path::new("./data/fibonacci/fib100/fib100_public_input.json"),
-            Path::new("./data/fibonacci/fib100/fib100_private_input.json"),
-            Path::new("./data/fibonacci/fib100/cpu_air_prover_config.json"),
-            Path::new("./data/fibonacci/fib100/cpu_air_params.json"),
-            Path::new("./data/fibonacci/fib100/fib100_proof.json"),
-            Path::new("../data/stone/fibonacci/fib100-prover.json"),
-            1
-        )
-        .unwrap();
-
-        // let proof = read_proof_file(output_file.path());
-        // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
-
-        run_verifier_from_command_line(
-            Path::new("./data/fibonacci/fib100/fib100-proof.json"), 
-            Path::new("../data/stone/fibonacci/fib100-verifier.json"),
-            1
-        )
-        .expect("Proof file is valid");
+    fn fibonacci_100_test() -> Result<(), Box<dyn std::error::Error>> {
+        run_test_case("fibonacci", "fib100", "fibonacci/fib100-prover.json", "fibonacci/fib100-verifier.json")
     }
 
     #[rstest]
-    fn fibonacci_1000_test_run_prover_verifier_from_command_line() {
-        run_prover_from_command_line(
-            Path::new("./data/fibonacci/fib1000/fib1000_public_input.json"),
-            Path::new("./data/fibonacci/fib1000/fib1000_private_input.json"),
-            Path::new("./data/fibonacci/fib1000/cpu_air_prover_config.json"),
-            Path::new("./data/fibonacci/fib1000/cpu_air_params.json"),
-            Path::new("./data/fibonacci/fib1000/fib1000_proof.json"),
-            Path::new("../data/stone/fibonacci/fib1000-prover.json"),
-            1
-        )
-        .unwrap();
-
-        // let proof = read_proof_file(output_file.path());
-        // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
-
-        run_verifier_from_command_line(
-            Path::new("./data/fibonacci/fib1000/fib1000-proof.json"), 
-            Path::new("../data/stone/fibonacci/fib1000-verifier.json"),
-            1
-        )
-        .expect("Proof file is valid");
+    fn fibonacci_1000_test() -> Result<(), Box<dyn std::error::Error>> {
+        run_test_case("fibonacci", "fib1000", "fibonacci/fib1000-prover.json", "fibonacci/fib1000-verifier.json")
     }
 
     #[rstest]
-    fn fibonacci_10_test_run_prover_verifier_from_command_line() {
-        run_prover_from_command_line(
-            Path::new("./data/fibonacci/fib10000/fib10000_public_input.json"),
-            Path::new("./data/fibonacci/fib10000/fib10000_private_input.json"),
-            Path::new("./data/fibonacci/fib10000/cpu_air_prover_config.json"),
-            Path::new("./data/fibonacci/fib10000/cpu_air_params.json"),
-            Path::new("./data/fibonacci/fib10000/fib10000_proof.json"),
-            Path::new("../data/stone/fibonacci/fib10000-prover.json"),
-            1
-        )
-        .unwrap();
-
-        // let proof = read_proof_file(output_file.path());
-        // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
-
-        run_verifier_from_command_line(
-            Path::new("./data/fibonacci/fib10000/fib10000-proof.json"), 
-            Path::new("../data/stone/fibonacci/fib10000-verifier.json"),
-            1
-        )
-        .expect("Proof file is valid");
+    fn fibonacci_10000_test() -> Result<(), Box<dyn std::error::Error>> {
+        run_test_case("fibonacci", "fib10000", "fibonacci/fib10000-prover.json", "fibonacci/fib10000-verifier.json")
     }
 }
+
+// #[cfg(test)]
+// mod test {
+//     use rstest::rstest;
+//     use super::*;
+
+//     #[rstest]
+//     fn sha256_8bytes_test_run_prover_verifier_from_command_line() {
+//         run_prover_from_command_line(
+//             Path::new("./data/sha256/8bytes/public_input.json"),
+//             Path::new("./data/sha256/8bytes/private_input.json"),
+//             Path::new("./data/sha256/8bytes/cpu_air_prover_config.json"),
+//             Path::new("./data/sha256/8bytes/cpu_air_params.json"),
+//             Path::new("./data/sha256/8bytes/proof.json"),
+//             Path::new("../benchmarks/stone/sha256/8B-prover.json"),
+//             1
+//         )
+//         .unwrap();
+
+//         // let proof = read_proof_file(output_file.path());
+//         // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
+
+//         run_verifier_from_command_line(
+//             Path::new("./data/sha256/8bytes/proof.json"), 
+//             Path::new("../benchmarks/stone/sha256/8B-verifier.json"),
+//             1
+//         )
+//         .expect("Proof file is valid");
+//     }
+
+//     #[rstest]
+//     fn fibonacci_1_test_run_prover_verifier_from_command_line() {
+//         run_prover_from_command_line(
+//             Path::new("./data/fibonacci/fib1/public_input.json"),
+//             Path::new("./data/fibonacci/fib1/private_input.json"),
+//             Path::new("./data/fibonacci/fib1/cpu_air_prover_config.json"),
+//             Path::new("./data/fibonacci/fib1/cpu_air_params.json"),
+//             Path::new("./data/fibonacci/fib1/proof.json"),
+//             Path::new("../benchmarks/stone/fibonacci/fib1-prover.json"),
+//             1
+//         )
+//         .unwrap();
+
+//         // let proof = read_proof_file(output_file.path());
+//         // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
+
+//         run_verifier_from_command_line(
+//             Path::new("./data/fibonacci/fib1/proof.json"), 
+//             Path::new("../benchmarks/stone/fibonacci/fib1-verifier.json"),
+//             1
+//         )
+//         .expect("Proof file is valid");
+//     }
+
+//     #[rstest]
+//     fn fibonacci_10_test_run_prover_verifier_from_command_line() {
+//         run_prover_from_command_line(
+//             Path::new("./data/fibonacci/fib10/public_input.json"),
+//             Path::new("./data/fibonacci/fib10/private_input.json"),
+//             Path::new("./data/fibonacci/fib10/cpu_air_prover_config.json"),
+//             Path::new("./data/fibonacci/fib10/cpu_air_params.json"),
+//             Path::new("./data/fibonacci/fib10/proof.json"),
+//             Path::new("../benchmarks/stone/fibonacci/fib10-prover.json"),
+//             1
+//         )
+//         .unwrap();
+
+//         // let proof = read_proof_file(output_file.path());
+//         // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
+
+//         run_verifier_from_command_line(
+//             Path::new("./data/fibonacci/fib10/proof.json"), 
+//             Path::new("../benchmarks/stone/fibonacci/fib10-verifier.json"),
+//             1
+//         )
+//         .expect("Proof file is valid");
+//     }
+
+//     #[rstest]
+//     fn fibonacci_100_test_run_prover_verifier_from_command_line() {
+//         run_prover_from_command_line(
+//             Path::new("./data/fibonacci/fib100/public_input.json"),
+//             Path::new("./data/fibonacci/fib100/private_input.json"),
+//             Path::new("./data/fibonacci/fib100/cpu_air_prover_config.json"),
+//             Path::new("./data/fibonacci/fib100/cpu_air_params.json"),
+//             Path::new("./data/fibonacci/fib100/proof.json"),
+//             Path::new("../benchmarks/stone/fibonacci/fib100-prover.json"),
+//             1
+//         )
+//         .unwrap();
+
+//         // let proof = read_proof_file(output_file.path());
+//         // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
+
+//         run_verifier_from_command_line(
+//             Path::new("./data/fibonacci/fib100/proof.json"), 
+//             Path::new("../benchmarks/stone/fibonacci/fib100-verifier.json"),
+//             1
+//         )
+//         .expect("Proof file is valid");
+//     }
+
+//     #[rstest]
+//     fn fibonacci_1000_test_run_prover_verifier_from_command_line() {
+//         run_prover_from_command_line(
+//             Path::new("./data/fibonacci/fib1000/public_input.json"),
+//             Path::new("./data/fibonacci/fib1000/private_input.json"),
+//             Path::new("./data/fibonacci/fib1000/cpu_air_prover_config.json"),
+//             Path::new("./data/fibonacci/fib1000/cpu_air_params.json"),
+//             Path::new("./data/fibonacci/fib1000/proof.json"),
+//             Path::new("../benchmarks/stone/fibonacci/fib1000-prover.json"),
+//             1
+//         )
+//         .unwrap();
+
+//         // let proof = read_proof_file(output_file.path());
+//         // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
+
+//         run_verifier_from_command_line(
+//             Path::new("./data/fibonacci/fib1000/proof.json"), 
+//             Path::new("../benchmarks/stone/fibonacci/fib1000-verifier.json"),
+//             1
+//         )
+//         .expect("Proof file is valid");
+//     }
+
+//     #[rstest]
+//     fn fibonacci_10_test_run_prover_verifier_from_command_line() {
+//         run_prover_from_command_line(
+//             Path::new("./data/fibonacci/fib10000/public_input.json"),
+//             Path::new("./data/fibonacci/fib10000/private_input.json"),
+//             Path::new("./data/fibonacci/fib10000/cpu_air_prover_config.json"),
+//             Path::new("./data/fibonacci/fib10000/cpu_air_params.json"),
+//             Path::new("./data/fibonacci/fib10000/proof.json"),
+//             Path::new("../benchmarks/stone/fibonacci/fib10000-prover.json"),
+//             1
+//         )
+//         .unwrap();
+
+//         // let proof = read_proof_file(output_file.path());
+//         // assert_eq!(proof.proof_hex, prover_cli_test_case.proof.proof_hex);
+
+//         run_verifier_from_command_line(
+//                 Path::new("./data/fibonacci/fib10000/proof.json"), 
+//                 Path::new("../benchmarks/stone/fibonacci/fib10000-verifier.json"),
+//                 1
+//             )
+//             .expect("Proof file is valid");
+//         }
+//     }
+    
